6dfdb251fbfd21755a51ab23c2621c17cbca8443
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2024-12-08 13:50:11 -0500

    Added support for pulling in the README for the long_description

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setup(
     url='https://github.com/retroandchill/ue5-tools',
     author='Retro & Chill',
     description='UE5 Python helper tools',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     install_requires=[
         'conan>=2.9.2'
     ],
